0b3566a3258e2f257bab80bc58108b451a804980
NohGiBong <nodarbong@gmail.com>
2024-03-15 16:40:46 +0900

    dd

---
+++ src/test/java/com/busanit/boot_board/BootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit.boot_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
